a04065c7bb406ec2df3a676fb810c737171c8214
Paul Gomersbach <p.gomersbach@rely.nl>
2016-04-06 19:13:39 +0200

    disable rspec_monitor

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,5 +12,5 @@ class role_lb
   # a role includes one or more profiles and at least a 'base' profile
   include ::profile_base
   # include rspec monitor to make rspec acceptance test available to monitor system
-  include ::profile_base::rspec_monitor
+  # include ::profile_base::rspec_monitor
 }
