93a9b9d4b6338aeef6a36d0a2affc14dff05f730
Paul Gomersbach <p.gomersbach@rely.nl>
2016-06-02 14:38:57 +0000

    enable rspec_monitor

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,8 @@ class role_lb
 {
   # a role includes one or more profiles and at least a 'base' profile
   include ::profile_base
-
+  # include rspec monitor to make rspec acceptance test available to monitor system
+  include ::profile_base::rspec_monitor
   include ::profile_haproxy
+  include ::profile_haproxy::rspec_monitor
 }
